646a7a76280fa77ed641f6d5e3ded691127b8666
alcohol-korea <han071416@naver.com>
2021-09-01 19:02:36 +0900

    gradient와 click js완성

---
+++ side-project/kakaoWebtoon/button/click.js
@@ -0,0 +1,25 @@
+const 
+icon = document.querySelectorAll("i"),
+button = document.querySelector(".responsive-btn");
+
+function clickedButton(e) {
+    for(i=0;i<5;i++){
+        icon[i].style.display = "none";
+        icon[2].style.display = "block";
+    }
+    button.style.width = "50px";
+    if(button.offsetWidth === 50){
+        button.style.width = "500px";
+        for(i=0;i<5;i++){
+            icon[i].style.display = "block";
+        }
+        return;
+    } 
+}
+
+icon[2].addEventListener("click", clickedButton);
+/*
+        window.addEventListener("click", (e)=>{
+            button.style.width = "500px";
+        });
+*/
